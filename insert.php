3400d2d8410ddf7d215c0660f6abd50a9e84b64f
annetsabu <annetsabu170@gmail.com>
2024-05-15 16:50:23 +0530

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,39 @@
+<?php
+require_once '../vendor/autoload.php';
+use App\classes\Session;
+Session::init();
+//USER INSERT
+if(isset($_POST['cat-btn'])){
+    $userOb = new \App\classes\Category();
+   $rtn_txt =  $userOb->addCategory($_POST);
+   Session::set('txt',"$rtn_txt");
+    header('location:addcategory.php');
+}
+if(isset($_POST['post-btn'])){
+    $userOb = new \App\classes\Post();
+   $rtn_txt =  $userOb->addPost($_POST);
+   Session::set('txt',"$rtn_txt");
+    header('location:addpost.php');
+}
+//USER INSERT
+if(isset($_POST['user-btn'])){
+    $userOb = new \App\classes\UserLogin();
+   $rtn_txt =  $userOb->addUser($_POST);
+   Session::set('userExists',"$rtn_txt");
+    header('location:adduser.php');
+}
+if(isset($_POST['user'])){
+    $userOb = new \App\classes\UserLogin();
+   $rtn_txt =  $userOb->addUser($_POST);
+   Session::set('userExists',"$rtn_txt");
+    header('location:index.php');
+}
+//reply INSERT
+if(isset($_POST['reply-btn'])){
+    $userOb = new \App\classes\Mail();
+    $rtn_txt =  $userOb->saveReply($_POST);
+    header('location:inbox.php');
+}
+
+
+?>
